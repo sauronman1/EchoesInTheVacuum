38756994b38b9e80bb11440fc3e61beddd75f534
GentoGaming <69847328+GentoGaming@users.noreply.github.com>
2021-03-05 16:20:16 +0100

    commiting changes
    sound

--- SDLTest - Copy/src/Game Engine/Essentials/SoundManager/SoundManager.cpp
+++ SDLTest - Copy/src/Game Engine/Essentials/SoundManager/SoundManager.cpp
@@ -1,1 +1,79 @@
 #include "SoundManager.h"
+#include "../../Essentials/CommonFunctions.h"
+
+SoundManager* SoundManager::s_Instance = nullptr;
+
+SoundManager::SoundManager() {
+
+	if (Mix_OpenAudio(FREQ, MIX_DEFAULT_FORMAT, 2, CHUNK_SIZE < 0))
+	{
+		printMixError(Mix_GetError());
+	}
+}
+
+void SoundManager::printMixError(std::string error)
+{
+	CommonFunctions::printErrorMessage(error);
+}
+
+
+	void SoundManager::playMusic(std::string id)
+	{
+		if(Mix_PlayMusic(musicClips[id], -1) == -1)
+			printMixError(Mix_GetError());
+	}
+
+
+	void SoundManager::musicVolume(int volume)
+	{
+		Mix_VolumeMusic(volume);
+	}
+
+
+	void SoundManager::StopMusic(std::string id)
+	{
+		if (Mix_PlayMusic(musicClips[id], 0) == -1)
+			printMixError(Mix_GetError());
+	}
+
+	void SoundManager::loadMusic(std::string id, std::string source)
+	{
+		Mix_Music* music = Mix_LoadMUS(source.c_str());
+		if (music != nullptr) {
+			musicClips[id] = music;
+		}
+		else {
+			printMixError(Mix_GetError());
+
+		}
+	}
+	void SoundManager::playEffect(std::string id)
+	{
+		if(Mix_PlayChannel(-1,effectClips[id], 0) == -1)
+			printMixError(Mix_GetError());
+	}
+	void SoundManager::loadEffect(std::string id, std::string source)
+	{
+		Mix_Chunk* effect = Mix_LoadWAV(source.c_str());
+		if (effect != nullptr)
+			effectClips[id] = effect;
+		else
+			printMixError(Mix_GetError());
+	}
+
+	void SoundManager::cleanSounds()
+	{
+		for (MusicClips::iterator it = musicClips.begin();it != musicClips.end();it++) {
+			Mix_FreeMusic(it->second);
+		}
+
+		for (EffectClips::iterator it = effectClips.begin();it != effectClips.end();it++) {
+			Mix_FreeChunk(it->second);
+		}
+		musicClips.clear();
+		effectClips.clear();
+		CommonFunctions::printConsoleMessage("Sound System is Cleaned");
+
+	}
+
+
--- SDLTest - Copy/src/Game Engine/Essentials/SoundManager/SoundManager.h
+++ SDLTest - Copy/src/Game Engine/Essentials/SoundManager/SoundManager.h
@@ -1,5 +1,39 @@
 #pragma once
+#include <map>
+#include <string>
+#include <iostream>
+#include <SDL_mixer.h>
+
+#define FREQ 44100
+#define CHUNK_SIZE 2048
+
+using EffectClips = std::map<std::string, Mix_Chunk*>;
+using MusicClips = std::map<std::string, Mix_Music*>;
+
 class SoundManager
 {
+public:
+	inline static SoundManager* instance() {
+		return s_Instance = (s_Instance != nullptr) ? s_Instance : new SoundManager();
+	}
+
+	void StopMusic(std::string id);
+	void loadMusic(std::string, std::string source);
+	void playEffect(std::string id);
+	void loadEffect(std::string id, std::string source);
+	void cleanSounds();
+	void playMusic(std::string id);
+	// Volume from 0 to 128 as per documentation 
+	void musicVolume(int volume);
+
+private:
+	SoundManager();
+	MusicClips musicClips;
+	EffectClips effectClips;
+	static SoundManager* s_Instance;
+	void printMixError(std::string error);
+
+
+
 };
 
